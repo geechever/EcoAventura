9eab064f0bfcd5acdf161b51b05c776c5c5f852d
geechever <45499245+geechever@users.noreply.github.com>
2025-07-14 16:37:44 -0400

    Delete project.godot directory

--- project.godot/EcoAventura/scripts/core/label.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-[node name="Title" type="Label"]
-text = "ECOAVENTURA"
-custom_fonts/font = FontFile
-custom_fonts/font.font_data = ExtResource("res://assets/fonts/comic.ttf")
-custom_fonts/font_size = 72
-custom_colors/font_color = Color(1, 1, 1, 1)
